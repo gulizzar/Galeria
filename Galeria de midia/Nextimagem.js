a74078d2a7c52126313e6db7f3696aeb14975724
gt_l15 <88586466+gulizzar@users.noreply.github.com>
2021-09-07 05:14:14 -0300

    Add files via upload

---
+++ Galeria de midia/Nextimagem.js
@@ -0,0 +1,12 @@
+function next_image1() {
+$ ("#img_1").addClass("d-none");
+$ ("#img_2").removeClass("d-none");
+}
+function next_image2() {
+    $ ("#img_2").addClass("d-none");
+    $ ("#img_3").removeClass("d-none");
+    }
+    function next_image3() {
+        $ ("#img_3").addClass("d-none");
+        $ ("#img_1").removeClass("d-none");
+        }
